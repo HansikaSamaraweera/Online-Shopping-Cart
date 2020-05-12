aedd28fe0b605ea05006c0ee631ac0001c0f55ba
OshaniMalavige <malavigeoshani@gmail.com>
2020-05-12 05:25:24 -0700

    Category domain

--- Backend-master/src/main/java/com/example/pro/demopro/domain/Category.java
+++ Backend-master/src/main/java/com/example/pro/demopro/domain/Category.java
@@ -1,7 +1,49 @@
 package com.example.pro.demopro.domain;
 
-public class Category {
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
 
-    private String name;
+@Document(collection = "Category")
+public class Category {
+    @Id
+    private String id;
+    private String cName;
+    private String cType;
+    private String cDate;
+
+    public Category(){
+
+    }
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getcName() {
+        return cName;
+    }
+
+    public void setcName(String cName) {
+        this.cName = cName;
+    }
+
+    public String getcType() {
+        return cType;
+    }
+
+    public void setcType(String cType) {
+        this.cType = cType;
+    }
+
+    public String getcDate() {
+        return cDate;
+    }
+
+    public void setcDate(String cDate) {
+        this.cDate = cDate;
+    }
 
 }
