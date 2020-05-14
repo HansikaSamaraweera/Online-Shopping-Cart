6f00df8895465b200c595fd917ee26973f72daba
OshaniMalavige <malavigeoshani@gmail.com>
2020-05-13 21:18:44 -0700

    offers

--- Frontend-master/src/components/Category/Offers.js
+++ Frontend-master/src/components/Category/Offers.js
@@ -1,10 +1,42 @@
-import React, {Component} from "react";
+import React from 'react';
+import c1 from "../images/c1.PNG"
+import {MDBBtn, MDBCard, MDBCardBody, MDBCardImage, MDBCardTitle, MDBCardText, MDBCol, MDBRow} from 'mdbreact';
+
+const Offers = () => {
+    return (
+        <MDBRow>
+            <MDBCard style={{ width: "26rem" }}>
+                <MDBCardImage className="img-fluid" src={c1} waves />
+                <MDBCardBody>
+                    <MDBCardTitle>Rs.2900/=</MDBCardTitle>
+                    <MDBCardText>
+                        Offer Price:Rs.2140/=
+                    </MDBCardText>
+                </MDBCardBody>
+            </MDBCard>
+            <MDBCard style={{ width: "26rem" }}>
+                <MDBCardImage className="img-fluid" src={c1} waves />
+                <MDBCardBody>
+                    <MDBCardTitle>Rs.2900/=</MDBCardTitle>
+                    <MDBCardText>
+                        Offer Price:Rs.2140/=
+                    </MDBCardText>
+                </MDBCardBody>
+            </MDBCard>
+            <MDBCard style={{ width: "26rem" }}>
+                <MDBCardImage className="img-fluid" src={c1} waves />
+                <MDBCardBody>
+                    <MDBCardTitle>Rs.2900/=</MDBCardTitle>
+                    <MDBCardText>
+                        Offer Price:Rs.2140/=
+                    </MDBCardText>
+                </MDBCardBody>
+            </MDBCard>
+
+        </MDBRow>
 
 
-
-class Offers extends Component {
-
-    render() {
-    }
+    )
 }
+
 export default Offers;
