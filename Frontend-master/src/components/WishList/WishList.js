5db0f35d5ad6a81bd9c352dde580f44fac0fa7e3
OshaniMalavige <malavigeoshani@gmail.com>
2020-05-16 04:22:28 -0700

    f4

--- Frontend-master/src/components/WishList/WishList.js
+++ Frontend-master/src/components/WishList/WishList.js
@@ -1,54 +1,49 @@
 import React, {Component} from 'react';
 import axios from 'axios';
-import qs from "query-string";
 
+const WishLists = props => (
 
-const Message = props => (
-    <p>{props.product.name}</p>
+    <p>{props.todo.productName}</p>
+
 
 )
-class WishList extends Component{
+class WishList extends Component {
     constructor(props) {
         super(props);
         this.state = {product:[]};
     }
-
     componentDidMount() {
-
-        axios.get('http://localhost:8080/api/Products/'+qs.parse(this.props.location.search, { ignoreQueryPrefix: true })._k)
-            .then(response =>{
-                this.setState({
-                    name : response.data.name
-
-                });
-                console.log(this.state.name);
+        axios.get("http://localhost:8080/wishList/get")
+            .then(response=>{
+                this.setState({product: response.data});
             })
             .catch(function (error) {
                 console.log(error)
             })
     }
-    wishList(){
-        return this.state.product.map(function (current,i) {
-            return <Message todo={current} key={i}/>;
+    wList() {
+        return this.state.product.map(function (current, i) {
+            return <WishLists todo={current} key={i}/>;
         });
     }
+
     render() {
         return (
             <div className={"container"}>
-                <div className={"row"}>
-                    <div className={"col"}>
                         <div className="card card-body my-3 col-sm-4">
                             <div className="card-body">
-                                {this.wishList()}
+                                {this.wList()}
                                 <button type="button" className="btn btn-danger text-white btn-block">Add To Cart
                                 </button>
                                 <button type="button" className="btn btn-danger text-white btn-block">Remove</button>
                             </div>
                         </div>
-                    </div>
-                </div>
             </div>
-        );
+        )
     }
 }
 export default WishList;
+
+
+
+
