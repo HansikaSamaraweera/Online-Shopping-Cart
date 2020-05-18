229a03127ed78fe64963efa2ec29b1397a38bf47
OshaniMalavige <malavigeoshani@gmail.com>
2020-05-18 09:18:39 -0700

    f6

--- Frontend-master/src/actions/types.js
+++ Frontend-master/src/actions/types.js
@@ -4,6 +4,7 @@ export const GET_USER="GET_USER";
 export const DELETE_PRODUCT = "DELETE_PRODUCT";
 export const GET_PRODUCT="GET_PRODUCT";
 export const GET_PRODUCT1="GET_PRODUCT1";
+export const DELETE_ACCOUNT = "DELETE_ACCOUNT";
 
 
 
