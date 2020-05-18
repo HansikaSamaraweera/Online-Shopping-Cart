69940dfa88965d1b8dca47372e3ba98616e551e4
OshaniMalavige <malavigeoshani@gmail.com>
2020-05-18 09:21:44 -0700

    ep

--- Frontend-master/src/components/ProjectTask/EditProfile.js
+++ Frontend-master/src/components/ProjectTask/EditProfile.js
@@ -5,109 +5,81 @@ import {
     getUser,
     addNewUserCus,
 } from "../../actions/projectTaskActions";
+import axios from "axios";
 
 class EditProfile extends Component{
     constructor(props) {
         super(props);
         this.state = {
-            name:"",
-            email:"",
-            errors:[]
+            user_prof: {}
         };
-        this.onChange = this.onChange.bind(this);
-        this.onSubmit = this.onSubmit.bind(this);
-    }
-
-    componentWillReceiveProps(nextProps) {
-        if (nextProps.errors) {
-            this.setState({ errors: nextProps.errors });
-        }
-
-        /*const { id, name,email } =nextProps.user;
-
-        this.setState({
-            id,
-            name,
-            email
-        });*/
     }
 
     componentDidMount() {
-        const { id } = this.props.match.params;
-        this.props.getUser(id);
+        axios.get('/api/Users/name/${sessionStorage.getItem("sessionName")')
+            .then(res => {
+                this.setState({ user_prof: res.data });
+                console.log(this.state.user_prof);
+            });
     }
 
-    onSubmit(e) {
+    onChange = (e) => {
+        this.setState({
+            name: e.target.value,
+            email: e.target.value
+        });
+    }
+
+    onSubmit = (e) => {
         e.preventDefault();
-        const updatedTask2 = {
-            id: this.state.id,
-            name: this.state.name,
-            email: this.state.email
-        };
 
-        this.props.addNewUserCus(updatedTask2, this.props.history);
+        const { name, email } = this.state.user_prof;
+
+        axios.put('/api/Users/'+this.props.match.params.id, { name, email })
+            .then((result) => {
+                this.props.history.push("/"+this.props.match.params.id)
+            });
     }
 
-    onChange(e) {
-        this.setState({ [e.target.name]: e.target.value });
-    }
+
 
     render() {
         return(
-            <div className="">
-                <br/>
-                <div className="col-md-6 m-auto alert-info">
-                    <div className="container">
-                        <div className="row">
-                            <div className="col-md-9 m-auto">
-                                <h6 className="display-5 text-center">Edit Profile</h6><br/>
-                                <form onSubmit={this.onSubmit}>
-                                    {/*name*/}
-                                    <div className="form-group">
-                                        <input type="text"
-                                               placeholder="User Name"
-                                               value={sessionStorage.getItem("sessionName")}
-                                               onChange={this.onChange}
-                                               required
-                                        />
+            <div className="col-md-6 m-auto alert-info">
+                <div className="container">
+                    <div className="row">
+                        <div className="col-md-9 m-auto">
+                <h3> Edit Profile </h3>
+                <form onSubmit={this.onSubmit}>
+                    <div className="form-group">
+                        <label>User Name: </label>
+                        <input type="text"
+                               className="form-control"
+                               name="name"
+                               value={this.state.user_prof.name}
+                               placeholder="User Name"
+                               onChange={this.onChange}
+                                />
+                    </div>
+                    <div className="form-group">
+                        <label>Email: </label>
+                        <input type="text"
+                               className="form-control"
+                               placeholder="Email"
+                               value={this.state.user_prof.email}
+                               onChange={this.onChange}
+                                 />
+                    </div>
+                    <div className="form-group">
+                        <input type="submit" value=" Update Profile" className="btn btn-primary"/>
 
-                                    </div>
-
-                                    {/*email*/}
-                                    {/*<div className="form-group">
-                                        <input type="text"
-                                               id="mail"
-                                               name="email"
-                                               value={this.state.email}
-                                               placeholder="Email"
-                                               onChange={this.onChange}
-                                               required
-                                        />
-                                    </div>*/}
-                                    <input type="submit" className="btn btn-outline-success btn-block col-md-6 m-auto" value="Save"/>
-                                </form>
-                                <p><br/><br/><br/></p>
-                            </div>
-                        </div>
+                    </div>
+                </form>
+            </div>
                     </div>
                 </div>
             </div>
         );
     }
 }
-EditProfile.propTypes = {
-    user_task: PropTypes.object.isRequired,
-    errors: PropTypes.object.isRequired,
-    getUser: PropTypes.func.isRequired,
-    addNewUserCus: PropTypes.func.isRequired
-};
-
-const mapStateToProps = state => ({
-    user_task: state.user_task.user_task,
-    errors: state.errors
-});
-
-export default connect(
-    mapStateToProps,
-    { getUser, addNewUserCus}
-)(EditProfile);
+export default EditProfile;
