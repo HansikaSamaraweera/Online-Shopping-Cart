934484ec213c2d2c8023c96f910716f12db96b8d
OshaniMalavige <malavigeoshani@gmail.com>
2020-05-18 09:13:20 -0700

    wish Rep

--- Backend-master/src/main/java/com/example/pro/demopro/repositary/WishListRepository.java
+++ Backend-master/src/main/java/com/example/pro/demopro/repositary/WishListRepository.java
@@ -4,5 +4,8 @@ import com.example.pro.demopro.domain.WishList;
 import org.springframework.data.mongodb.repository.MongoRepository;
 
 public interface WishListRepository extends MongoRepository<WishList,String> {
+
+    Iterable<WishList> getByUser(String user);
+
     WishList getById(String id);
 }
