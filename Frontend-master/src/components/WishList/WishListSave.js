fabaf2f3ac958735392f256abd1df40ff452ed6c
OshaniMalavige <malavigeoshani@gmail.com>
2020-05-21 02:55:13 -0700

    wish list to add to cart

--- Frontend-master/src/components/WishList/WishListSave.js
+++ Frontend-master/src/components/WishList/WishListSave.js
@@ -1,12 +1,11 @@
 import React, {Component} from 'react';
-import WishList from "./WishList";
 import PropTypes from "prop-types";
 import {connect} from "react-redux";
 import {addToWishList} from "../../actions/actions";
 import axios from "axios";
 import qs from 'query-string';
 import {Link} from "react-router-dom";
-import classnames from "classnames";
+
 
 class WishListSave extends Component {
     constructor(props) {
@@ -49,7 +48,7 @@ class WishListSave extends Component {
                 price: this.state.price
             };
             this.props.addToWishList(newWishList, this.props.history)
-            alert("Added to WishList!")
+            alert("Save to WishList!")
         }
     }
 
@@ -62,10 +61,10 @@ class WishListSave extends Component {
         return(
         <div className="col-md-7 m-auto">
             <div className="container">
+                <h2 className="text-capitalize card card-body bg-info text-white "> My Wish List</h2>
                 <div className="row">
                     <div className="col-md-9 m-auto">
                         <br/>
-                        <h1 className="text-center" id="tit"> My Wish List</h1>
                         <div className="card card-body my-5 col-sm-11">
                             <div className="card-body">
                                 <h5 className="card-title">Item Name:{this.state.name} </h5>
@@ -73,9 +72,9 @@ class WishListSave extends Component {
                             </div>
                             <div className="card-body">
                                 <form onSubmit={this.onSubmit}>
-                                    <button type="submit" className="btn btn-primary btn-block" onClick={'/WhishList_Admin/WishList'+this.state.id} >Save</button>
-                                    <Link to="/WhishList_Admin/WishList" className="btn btn-block btn-danger m-auto">
-                                        More Actions <i className="fas fa-arrow-alt-circle-right"></i>
+                                    <button type="submit" className="btn btn-info btn-block" onClick={'/WhishList_Admin/WishList'+this.state.id} >SAVE TO WISHLIST</button>
+                                    <Link to="/" className="btn btn-block btn-danger m-auto">
+                                        <i className="fas fa-arrow-alt-circle-left"></i>  RETURN TO SHOP
                                     </Link>
                                 </form>
                             </div>
