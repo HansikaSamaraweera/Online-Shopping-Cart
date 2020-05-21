2058b0cf3c8a355f9ea0a0d59e58107e40bbe74e
OshaniMalavige <malavigeoshani@gmail.com>
2020-05-21 03:00:07 -0700

    comment actions

--- Frontend-master/src/actions/categoryModule.js
+++ Frontend-master/src/actions/categoryModule.js
@@ -1,15 +1,12 @@
 import axios from "axios";
-import {
-    GET_ERRORS,
-    GET_CATEGORY
-} from "./types";
-import category from "../reducers/category";
+import {GET_ERRORS,GET_CATEGORY, GET_UPDATE_CATEGORY} from "./moduleTypes";
 
 {/*Add New Category*/}
 export const addCategory = (category, history) => async dispatch => {
     try {
         await axios.post("/api/categories", category);
         history.push("/");
+        window.location.replace("/AdminAsCategory/AddCategory")
         dispatch({
             type: GET_ERRORS,
             payload: {}
@@ -39,10 +36,26 @@ export const updateCategory=(category,history)=>async dispatch=>{
     }
 };
 
-/*export const getAllCategories = () => async dispatch => {
-    const res = await axios.get("http://localhost:8080/api/categories/all");
+{/*Get category by id*/}
+export const getCategory = (id, history) => async dispatch =>{
+    try{
+        const res = await axios.get('/api/categories/${id}');
+        dispatch({
+            type: GET_UPDATE_CATEGORY,
+            payload: res.data
+        });
+
+    }catch (error) {
+        history.push("/CategoryList")
+
+    }
+};
+
+{/*Display All Categories*/}
+export  const getAllCategories = () => async  dispatch => {
+    const res = await axios.get("/api/categories/all");
     dispatch({
-        type: GET_CATEGORY,
-        payload: res.data
-    });
-};*/
+        type:GET_CATEGORY,
+        payload:res.data
+    })
+};
