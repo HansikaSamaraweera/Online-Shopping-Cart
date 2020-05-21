c3b1e82e0f3d607cba817394cd1c3c8f68db34f8
OshaniMalavige <malavigeoshani@gmail.com>
2020-05-21 02:52:45 -0700

    wish list admin updated

--- Frontend-master/src/WhishList_Admin.js
+++ Frontend-master/src/WhishList_Admin.js
@@ -4,6 +4,7 @@ import {BrowserRouter as Router,Route} from "react-router-dom";
 import {Provider} from "react-redux";
 import WishListSave from "./components/WishList/WishListSave";
 import WishList from "./components/WishList/WishList";
+import ViewCart from "./components/Product/ViewCart";
 
 
 class WhishList_Admin extends Component {
@@ -24,6 +25,7 @@ class WhishList_Admin extends Component {
                                 <div className="pre-scrollable blockquote h-25">
                                     <Route exact path="/WhishList_Admin/WishListSave" component={WishListSave}/>
                                     <Route exact path="/WhishList_Admin/WishList" component={WishList}/>
+                                    <Route exact path="/ViewCart" component={ViewCart}/>
                                 </div>
                                 :<div className="text-center">
                                     <br/>
