8a9e2e053aea780a99b47088b8cde08ab9dccd7a
OshaniMalavige <malavigeoshani@gmail.com>
2020-05-21 20:46:38 -0700

    update category

--- Frontend-master/src/components/Category/CategoryList.js
+++ Frontend-master/src/components/Category/CategoryList.js
@@ -42,7 +42,7 @@ class CategoryList extends Component {
                         <td className="text-truncate" id="cs">{post.cType}</td>
                         <td className="text-truncate" id="cs">{post.cDate}</td>
                         <td>
-                            <Link to={"/AdminAsCategory/EditCategory"}><i className="fas fa-edit"></i></Link>     <i className="fas fa-trash" onClick={this.onDeleteClick.bind(this,post.id)}></i>
+                            <Link to={"/AdminAsCategory/EditCategory/id?_k="+post.id}><i className="fas fa-edit"></i></Link>     <i className="fas fa-trash" onClick={this.onDeleteClick.bind(this,post.id)}></i>
                     </td>
                     </tr>
                 )
